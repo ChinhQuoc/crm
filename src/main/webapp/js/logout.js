2f5421e78ece51ce28fee45c5dd458387f019cbe
DESKTOP-RNK2HL9\DELL <dthiep.gm@gmail.com>
2023-09-29 23:38:49 +0700

    assignment/18-08-2023

--- src/main/webapp/js/logout.js
+++ src/main/webapp/js/logout.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
 	$(".btn-yes").click(function() {
-		let origin = window.location.origin;
-		window.location.replace(origin + "/login");
+		window.location.replace("http://localhost:8080/crm_project/login");
 	})
 })
